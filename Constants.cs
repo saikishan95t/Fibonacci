fcc7820bb880d479a78369e08f8c0b160095c36c
saikishan95t <70094267+saikishan95t@users.noreply.github.com>
2020-12-02 20:02:15 +0000

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Constants
+    {
+        public static int first_number = -1;
+        public static int second_number = 1;
+        public static int sum = 0;
+    }
+}
---
+++ Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Fibonacci : Constants
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the fibonacci number you want:");
+            int fibonacci_n = Convert.ToInt32(Console.ReadLine());
+            getFibonacciNumber(fibonacci_n);
+            Console.WriteLine("The " + fibonacci_n + "th fibonacci number is " + sum);
+        }
+        private static int getFibonacciNumber(int fibonacci_n)
+        {
+            for (int i = 0; i < fibonacci_n; i++)
+            {
+                sum = first_number + second_number;
+                first_number = second_number;
+                second_number = sum;
+            }
+            return sum;
+        }
+    }
+}
